59f589f9b48abb62dc42d67cd4fdfe74fb713d06
EzMiSh <166328379+EzMiSh@users.noreply.github.com>
2024-05-06 10:51:08 -0300

    sim

--- Atividade_Pratica_02/Hello_World.py
+++ Atividade_Pratica_02/Hello_World.py
@@ -15,3 +15,23 @@ Por último, opcionalmente, é adicionada a palavra reservada return para devolv
 Uma vez que a função é definida, você pode reutilizá-la várias vezes no seu programa.
 
 Agora, como exemplo, podemos criar uma função simples de soma com dois parâmetros:
+#print ( Usado para imprimir um comando na tela)
+#input ( Abre um terminar que permite ser digitado algo que será passado para a função ) 
+#.upper( usado para deixa todas as letras digitadas para responder o algoritmo "maiúsculas", os caracteres da string) 
+#While( Ela analisa se as instruções do bloco de nota e executa dando uma condição para o mesmo em forma de loop até a condição se atendida ou até a função break)
+#break( encerra o loop da função While )
+#if( Ela analisa se as instruções do bloco de nota e executa dando uma condição para o mesmo). 
+#elif( Serve como condição para executar uma regra. Deve ser usado depois do comando If) ( chama de cláusula If,While,elif,Else )
+#else( serve para executar uma ação, caso não esteja condicionada pelos comandos if e elif , sendo sempre usados depois deles, executando caso a comparação if/elif não passe)
+#float( utilizado para lidar com números decimais)
+#.f ( Usado para específica a quantidade de casas depois da vírgula) 
+# == ( igual a) 
+# * ( multiplicação)
+#!=( diferente )
+#len( Informa o número de caracteres ).
+#Parâmetro ( O que está entre parênteses)
+#Argumento ( A função definida para o parâmetro)
+# String ( Palavras entre aspas)
+# inteiros ( números inteiros, positivos e negativos )
+# float ( números racionais, que tem vírgulas )
+# variável ( palavra a ser especificado um argumento/ parâmetro)
