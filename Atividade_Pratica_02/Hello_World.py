42f1844d414000c2f982eec145b36a606216bfef
EzMiSh <166328379+EzMiSh@users.noreply.github.com>
2024-05-06 10:23:33 -0300

    Sim

--- Atividade_Pratica_02/Hello_World.py
+++ Atividade_Pratica_02/Hello_World.py
@@ -5,3 +5,13 @@
 print('Hello World')#Amostre a frase Hello World
 
 #print ( Usado para imprimir um comando na tela)
+
+#Para criar uma nova função em Python, a palavra reservada def é utilizada.
+Em seguida, é atribuído um nome ao identificador que será usado para chamá-la.
+Depois do nome, uma lista opcional de parâmetros é incluída entre parênteses.
+O cabeçalho da função termina com dois pontos.
+Nas linhas seguintes, com uma indentação maior vem o corpo da função, ou seja, uma sequência de sentenças que executam uma operação.
+Por último, opcionalmente, é adicionada a palavra reservada return para devolver um resultado.
+Uma vez que a função é definida, você pode reutilizá-la várias vezes no seu programa.
+
+Agora, como exemplo, podemos criar uma função simples de soma com dois parâmetros:
